ee185ea8f725d5b15293454b909f71bbc33a652f
Shengye Wang <i@shengye.wang>
2017-10-03 15:07:16 -0700

    Removes the clicking sound in playback microphone node

--- gcloud_speech_utils/scripts/playback_microphone_audio.py
+++ gcloud_speech_utils/scripts/playback_microphone_audio.py
@@ -9,7 +9,7 @@ import gcloud_speech_msgs.msg as gcloud_speech_msgs
 
 LinearPcm16Le16000Audio = gcloud_speech_msgs.LinearPcm16Le16000Audio
 
-FRAME_PER_SLICE=1600
+MSG_FRAME_PER_SLICE = 1600
 
 _audio_queue = collections.deque()
 
@@ -20,14 +20,20 @@ def RosCallback(msg):
 
 
 def PaCallback(in_data, frame_count, time_info, status):
-  assert frame_count == FRAME_PER_SLICE
+  assert frame_count == MSG_FRAME_PER_SLICE * 2
 
   while True:
     try:
-      data = _audio_queue.popleft()
+      # For some reason putting in 10 chunks per second to PyAudio introduces a
+      # lot of clicking sound. This hack only combines two chunks so that only
+      # PyAudio only needs to handle 5 chunks per seconds, and the clicking
+      # sound went alway.
+      data_1 = _audio_queue.popleft()
+      data_2 = _audio_queue.popleft()
+      data = data_1 + data_2
       break
     except:
-      time.sleep(.5)
+      time.sleep(1)
 
   return (data, pyaudio.paContinue)
 
@@ -40,7 +46,7 @@ def PlaybackMicrophoneAudio():
     pa = pyaudio.PyAudio()
     stream = pa.open(
       format=pyaudio.paInt16, channels=1, rate=16000, output=True,
-      stream_callback=PaCallback, frames_per_buffer=FRAME_PER_SLICE)
+      stream_callback=PaCallback, frames_per_buffer=MSG_FRAME_PER_SLICE * 2)
 
     stream.start_stream()
     rospy.spin()
